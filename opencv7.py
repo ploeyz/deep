93d0b0cc6db37914fe844f96f4a87296e4c13362
maneerin <sk.maneerin@gmail.com>
2025-07-25 13:28:14 +0700

    draw rectangle

---
+++ opencv7.py
@@ -0,0 +1,9 @@
+import cv2
+
+img = cv2.imread('photo/chelsea.jpg')
+
+cv2.rectangle(img, (100, 100), (150, 150), (250, 255, 50), 6)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
