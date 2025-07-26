410ca5621f37db9ec6edeac2f836b820a456cdce
maneerin <sk.maneerin@gmail.com>
2025-07-26 17:27:50 +0700

    clear

--- opencv.py
+++
@@ -1,7 +0,0 @@
-import cv2
-# read image from file
-img = cv2.imread('photo/Chelsea.jpg')
-cv2.imshow('PHOTO', img)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
--- opencv2.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-img = cv2.imread('photo/chelsea.jpg')
-cv2.imwrite('photo/newchelsea.jpg', img)
-print("Image saved successfully.")
